10da6e428e76bcf372179082a6e96ac989c00713
Ubuntu <ubuntu@jmat11-ubuntudesk.novalocal>
2019-12-02 19:37:43 -0400

    Commit about creation of testClass of each project class

---
+++ src/test/java/funda/prog101/PotTest.java
@@ -0,0 +1,14 @@
+package test.java.funda.prog101;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PotTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

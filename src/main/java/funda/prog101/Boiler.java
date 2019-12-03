818cd2407241459ce149a0d4cda96652e260756f
Ubuntu <ubuntu@jmat11-ubuntudesk.novalocal>
2019-12-02 21:17:04 -0400

    Commit boiler unit test for test boiler constructor

--- src/main/java/funda/prog101/Boiler.java
+++ src/main/java/funda/prog101/Boiler.java
@@ -1,11 +1,20 @@
 package main.java.funda.prog101;
 
 public class Boiler extends Container{
-
+	
 	@Override
 	public String addCups() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	private byte temperature;
+	private boolean deliveryPipe;
+	
+	public Boiler(TypeContent type, byte capacity, byte temperature,
+					boolean deliveryPipe) {
+		super(type, capacity);
+		this.temperature = temperature;
+		this.deliveryPipe = deliveryPipe;
+	}
 }
--- src/test/java/funda/prog101/BolierTest.java
+++ src/test/java/funda/prog101/BolierTest.java
@@ -1,13 +1,18 @@
 package test.java.funda.prog101;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import main.java.funda.prog101.Boiler;
 
 public class BolierTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBoilerConstructor() {
+		Boiler boiler1 = new Boiler(TypeContent.WATER, 12, 2, true);
+		assertEquals(boiler1.TypeContent, TypeContent.WATER);
+		assertEquals(boiler1.capacity, 12);
+		assertEquals(boiler1.getTemperature(), 2);
+		assertEquals(boiler1.getDeliveryPipe(), true);
 	}
 
 }

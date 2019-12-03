db76b79a9e712c02c00b30592d0ad282436341f1
Ubuntu <ubuntu@aeat11-ubuntudesk.novalocal>
2019-12-02 20:56:58 -0400

    Units test turn ON/OFF implemented_Alan

--- src/main/java/funda/prog101/Heater.java
+++ src/main/java/funda/prog101/Heater.java
@@ -1,5 +1,20 @@
 package main.java.funda.prog101;
 
 public class Heater {
+	
+	boolean state;
+	Pot pot;
+	public Heater() {
+		state = false;
+	}
+	public void turnOn() {
+		this.state = true;
+	}
+	public void turnOff() {
+		this.state = false;
+	}
+	public boolean getState() {
+		return this.state;
+	}
 
 }
--- src/test/java/funda/prog101/HeaterTest.java
+++ src/test/java/funda/prog101/HeaterTest.java
@@ -3,11 +3,25 @@ package test.java.funda.prog101;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import main.java.funda.prog101.Heater;
+
 public class HeaterTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TurnOn_SetTurnON_True() {
+		Heater heater = new Heater();
+		boolean expected = true;
+		heater.turnOn();
+		boolean actual = heater.getState();
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void TurnOff_SetTurnOff_False() {
+		Heater heater = new Heater();
+		boolean expected = false;
+		heater.turnOff();
+		boolean actual = heater.getState();
+		assertEquals(expected, actual);
 	}
 
 }

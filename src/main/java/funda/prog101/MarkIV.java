d8b905ee2af7db198c61fbc429704da6621c1693
Ubuntu <ubuntu@jmat11-ubuntudesk.novalocal>
2019-12-04 13:20:13 -0400

    commit Mark class intanciation

--- src/main/java/funda/prog101/MarkIV.java
+++ src/main/java/funda/prog101/MarkIV.java
@@ -1,5 +1,19 @@
 package main.java.funda.prog101;
 
 public class MarkIV {
-
+	private boolean startButton;
+	private boolean light;
+	
+	public boolean getStartButton() {
+		return startButton;
+	}
+	
+	public boolean getLight() {
+		return light;
+	}
+	
+	public MarkIV(boolean startButton, boolean light) {
+		this.startButton = startButton;
+		this.light = light;
+	}
 }
--- src/test/java/funda/prog101/MarkIVTest.java
+++ src/test/java/funda/prog101/MarkIVTest.java
@@ -2,12 +2,14 @@ package test.java.funda.prog101;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import main.java.funda.prog101.MarkIV;
 
 public class MarkIVTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitMarkIV_MarkIVInited() {
+		MarkIV markIV = new MarkIV(true, true);
+		assertEquals(markIV.getStartButton(), true);
+		assertEquals(markIV.getLight(), true);
 	}
-
 }

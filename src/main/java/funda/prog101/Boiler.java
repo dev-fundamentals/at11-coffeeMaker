1c9ddb6637ae1956e5fd59bc518c5910b5562ed7
Ubuntu <ubuntu@jmat11-ubuntudesk.novalocal>
2019-12-04 10:28:48 -0400

    commit implementation turnOnPipe, turnOfPipe

--- src/main/java/funda/prog101/Boiler.java
+++ src/main/java/funda/prog101/Boiler.java
@@ -1,28 +1,44 @@
 package main.java.funda.prog101;
 
 public class Boiler extends Container{
+	private byte temperature;
+	private boolean statePipe;
+	
+	public ContentType getContentType() {
+		return contentType;
+	}
+	
+	public byte getCapacity() {
+		return capacity;
+	}
+	
+	public byte getTemperature() {
+		return temperature;
+	}
+	
+	public boolean getStatePipe() {
+		return statePipe;
+	}
+	
+	public Boiler(ContentType type, byte capacity, byte temperature,
+					boolean deliveryPipe) {
+		this.contentType = type;
+		this.capacity = capacity;
+		this.temperature = temperature;
+		this.statePipe = deliveryPipe;
+	}
+	
+	public void turnOnPipe(boolean state) {
+		statePipe = state;
+	}
+	
+	public void turnOffPipe(boolean state) {
+		statePipe = state;
+	}
 	
 	@Override
 	public String addCups() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	private byte temperature;
-	private boolean deliveryPipe;
-	
-	public Boiler(TypeContent type, byte capacity, byte temperature,
-					boolean deliveryPipe) {
-		super(type, capacity);
-		this.temperature = temperature;
-		this.deliveryPipe = deliveryPipe;
-	}
-	
-	public void turnOnPipe() {
-		deliveryPipe = true;
-	}
-	
-	public void turnOffPipe() {
-		deliveryPipe = false;
 	}
 }
--- src/test/java/funda/prog101/BolierTest.java
+++ src/test/java/funda/prog101/BolierTest.java
@@ -3,27 +3,30 @@ package test.java.funda.prog101;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import main.java.funda.prog101.Boiler;
+import main.java.funda.prog101.ContentType;
 
 public class BolierTest {
 
-	Boiler boiler1 = new Boiler(TypeContent.WATER, 12, 2, true);
+	byte contentType = 12;
+	byte temperature = 2;
+	Boiler boiler1 = new Boiler(ContentType.WATER, contentType, temperature, true);
 	@Test
 	public void testBoilerConstructor() {
-		assertEquals(boiler1.TypeContent, TypeContent.WATER);
-		assertEquals(boiler1.capacity, 12);
-		assertEquals(boiler1.getTemperature(), 2);
-		assertEquals(boiler1.getDeliveryPipe(), true);
+		assertEquals(boiler1, ContentType.WATER);
+		assertEquals(boiler1.getCapacity(), 12);
+		assertEquals(boiler1.getTemperature(), temperature);
+		assertEquals(boiler1.getStatePipe(), true);
 	}
 
 	@Test
 	public void testTurnOnPipeMethodToTrue() {
 		boiler1.turnOnPipe(true);
-		assertEquals(boiler1.getDeliveryPipe(), true);
+		assertEquals(boiler1.getStatePipe(), true);
 	}
 	
 	@Test
 	public void testTurnOffPipeMethodToFalse() {
 		boiler1.turnOffPipe(false);
-		assertEquals(boiler1.getDeliveryPipe(), false);
+		assertEquals(boiler1.getStatePipe(), false);
 	}
 }

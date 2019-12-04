46566f8c2f73ea4f7358227200a8341e7f224605
Ubuntu <ubuntu@jmat11-ubuntudesk.novalocal>
2019-12-04 16:25:01 -0400

    implemetation of powerMethod and its unit test

--- src/main/java/funda/prog101/MarkIV.java
+++ src/main/java/funda/prog101/MarkIV.java
@@ -1,19 +1,43 @@
 package main.java.funda.prog101;
 
 public class MarkIV {
-	private boolean startButton;
+	private boolean switchButton;
 	private boolean light;
-	
-	public boolean getStartButton() {
-		return startButton;
+	/**
+	 * This is MarkIV constructor
+	 * @param startButton sets an boolean date for starting markIv
+	 * @param light sets an boolean date to light
+	 */
+	public MarkIV(boolean switchButton) {
+		this.switchButton = switchButton;
+		this.light = switchButton;
 	}
 	
-	public boolean getLight() {
-		return light;
+	/**
+	 * This method is for setting startButton
+	 * @param start boolean parameter;
+	 */
+	public void powerButton(boolean switchButton) {
+		this.switchButton = switchButton;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarkIV other = (MarkIV) obj;
+		if (light != other.light)
+			return false;
+		if (switchButton != other.switchButton)
+			return false;
+		return true;
 	}
 	
-	public MarkIV(boolean startButton, boolean light) {
-		this.startButton = startButton;
-		this.light = light;
+	public boolean verifyStateMark(boolean state) {
+		return switchButton == state;
 	}
 }
--- src/test/java/funda/prog101/MarkIVTest.java
+++ src/test/java/funda/prog101/MarkIVTest.java
@@ -1,15 +1,31 @@
 package test.java.funda.prog101;
 
 import static org.junit.Assert.*;
+import java.util.concurrent.locks.Condition;
 import org.junit.Test;
 import main.java.funda.prog101.MarkIV;
 
 public class MarkIVTest {
-
+	
 	@Test
 	public void testInitMarkIV_MarkIVInited() {
-		MarkIV markIV = new MarkIV(true, true);
-		assertEquals(markIV.getStartButton(), true);
-		assertEquals(markIV.getLight(), true);
+		MarkIV markIV = new MarkIV(true);
+		MarkIV mark = new MarkIV(true);
+		assertTrue(markIV.equals(mark));
+	}
+	
+	@Test
+	public void testPowerButtonMethodToPowerOn() {
+		MarkIV markIV = new MarkIV(false);
+		markIV.powerButton(true);
+		assertTrue(markIV.verifyStateMark(true));
+	}
+	
+	@Test
+	public void testPowerButtonMethodToPowerOff() {
+		MarkIV markIV = new MarkIV(false);
+		markIV.powerButton(false);
+		boolean actual = markIV.verifyStateMark(false);
+		assertEquals(true, actual);
 	}
 }

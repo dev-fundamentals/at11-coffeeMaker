60e8bac196d601c9adf3cbbdb9495e7c72b934be
Ubuntu <ubuntu@prog-101-ol.novalocal>
2019-12-04 18:15:20 -0400

    update-class-boiler-methods

--- src/main/java/funda/prog101/Boiler.java
+++ src/main/java/funda/prog101/Boiler.java
@@ -1,9 +1,14 @@
 package main.java.funda.prog101;
 
 public class Boiler extends Container{
-
+	/** Override addCups methods
+	  Adds cups to the container when it has between 0 and 11, 
+	  maximum 12 cups*/
 	@Override
 	public void addCups(Container container) {
 		// TODO Auto-generated method stub
+		for (int i = container.quantity; i < container.limit; i++) {
+			container.quantity++;
+		}
 	}
 }
--- src/test/java/funda/prog101/BolierTest.java
+++ src/test/java/funda/prog101/BolierTest.java
@@ -3,11 +3,18 @@ package test.java.funda.prog101;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import main.java.funda.prog101.Boiler;
+import main.java.funda.prog101.Container;
+import main.java.funda.prog101.Pot;
+
 public class BolierTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addCups_AddCupsofContainerUntil12_True() {
+		Container boiler = new Boiler();
+		boiler.addCups(boiler);
+		var actual = boiler.equals(boiler);
+		assertTrue(actual);
 	}
 
 }

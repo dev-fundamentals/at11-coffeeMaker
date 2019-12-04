f1284e3df48a7e3acaa11597481c15caa7d69e96
Ubuntu <ubuntu@ecat11-ubuntudesktop.novalocal>
2019-12-04 14:18:13 -0400

    Implementation of Container abstract class

--- src/main/java/funda/prog101/CoffeeFilter.java
+++ src/main/java/funda/prog101/CoffeeFilter.java
@@ -3,9 +3,8 @@ package main.java.funda.prog101;
 public class CoffeeFilter extends Container{
 
 	@Override
-	public String addCups(Container container) {
+	public void addCups(Container container) {
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
--- src/main/java/funda/prog101/Pot.java
+++ src/main/java/funda/prog101/Pot.java
@@ -3,9 +3,7 @@ package main.java.funda.prog101;
 public class Pot extends Container{
 
 	@Override
-	public String addCups(Container container) {
+	public void addCups(Container container) {
 		// TODO Auto-generated method stub
-		return null;
 	}
-
 }

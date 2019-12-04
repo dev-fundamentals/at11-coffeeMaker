4ffb5aba1f5be1fb4c94ec071300147112cd6c27
Ubuntu <ubuntu@prog-101-ol.novalocal>
2019-12-04 17:10:16 -0400

    Update Class Pot, @Test and Methods

--- src/main/java/funda/prog101/Pot.java
+++ src/main/java/funda/prog101/Pot.java
@@ -1,9 +1,30 @@
 package main.java.funda.prog101;
 
-public class Pot extends Container{
-
+public class Pot extends Container {
+	protected boolean aboveHeater = true;
+	/*Adds cups to the container when it has between 0 and 11, maximum 12 cups*/
 	@Override
 	public void addCups(Container container) {
 		// TODO Auto-generated method stub
+
+		for (int i = container.quantity; i < container.limit; i++) {
+			container.quantity++;
+		}
+	}
+	/* Change the state of variable aboveHeater */
+	public void putInTakeOutPot() {
+		if (aboveHeater) {
+			aboveHeater = true;
+		} else {
+			aboveHeater = false;
+		}
+	}
+	/* Verify that the Pot is on the Heater, returns true is it, and false if not on the Heater.*/
+	public boolean equals(boolean statePot) {
+		if (statePot == this.aboveHeater) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- src/test/java/funda/prog101/PotTest.java
+++ src/test/java/funda/prog101/PotTest.java
@@ -3,11 +3,42 @@ package test.java.funda.prog101;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import main.java.funda.prog101.Container;
+import main.java.funda.prog101.Pot;
+
 public class PotTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addCups_AddCupsofContainer_12() {
+		var expected = 12;
+		Container pot = new Pot();
+		var actual = pot.addCups(pot);
+		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void addCups_AddCupsofContainer_0() {
+		var expected = 8;
+		Container pot = new Pot();
+		var actual = pot.addCups(pot);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void equals_verifyIfPotIsOnHeater_false() {
+		boolean expected = false;
+		Container pot = new Pot();
+		boolean statePot = false;
+		boolean actual = pot.equals(statePot);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void equals_verifyIfPotIsOnHeater_true() {
+		boolean expected = true;
+		Container pot = new Pot();
+		boolean statePot = false;
+		boolean actual = pot.equals(statePot);
+		assertEquals(expected, actual);
+	}
 }

fc6a525f5dc467b3ecda057c7c3e2a47364f90e6
Ubuntu <ubuntu@prog-101-ol.novalocal>
2019-12-04 11:00:12 -0400

    assigment classSensor isEmpty_oscar

--- src/main/java/funda/prog101/Sensor.java
+++ src/main/java/funda/prog101/Sensor.java
@@ -2,4 +2,8 @@ package main.java.funda.prog101;
 
 public class Sensor {
 
+	public boolean isEmpty(Container container) {
+			return false;
+	}
+	
 }
--- src/test/java/funda/prog101/SensorTest.java
+++ src/test/java/funda/prog101/SensorTest.java
@@ -3,11 +3,26 @@ package test.java.funda.prog101;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import main.java.funda.prog101.Container;
+
+import main.java.funda.prog101.Sensor;
+
 public class SensorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void IsEmpty_IfContainerNotEmpty_True() {
+		Sensor sensor = new Sensor();
+		Container container = null;
+		boolean result = sensor.isEmpty(container);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void IsEmpty_IfContainerIsEmpty_True() {
+		Sensor sensor = new Sensor();
+		Container container = null;
+		boolean result = sensor.isEmpty(container);
+		assertTrue(result);
 	}
 
 }

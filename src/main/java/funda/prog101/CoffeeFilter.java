ab7af5112ade75dd07a8f2e1f083f9d4ede4e461
Ubuntu <ubuntu@ecat11-ubuntudesktop.novalocal>
2019-12-05 15:29:13 -0400

    Update Pot class

--- src/main/java/funda/prog101/CoffeeFilter.java
+++ src/main/java/funda/prog101/CoffeeFilter.java
@@ -1,10 +1,15 @@
 package main.java.funda.prog101;
 
 public class CoffeeFilter extends Container{
+	
+	public CoffeeFilter() {
+		this.contentType = ContentType.COFFEEBEAN;
+		this.limit = 1;  
+	}
 
 	@Override
 	public void addCups(Container container) {
-		// TODO Auto-generated method stub
+		container.contentType = ContentType.COFFEE;
+		container.quantity = 1;
 	}
-
 }
--- src/main/java/funda/prog101/Pot.java
+++ src/main/java/funda/prog101/Pot.java
@@ -3,9 +3,18 @@ package main.java.funda.prog101;
 public class Pot extends Container {
 	protected boolean aboveHeater = true;
 
-	/*
-	 * Adds cups to the container when it has between 0 and 11, 
-	 * maximum 12 cups
+	public Pot(ContentType contentType, byte quantity, byte limit, boolean aboveHeater) {
+		this.contentType = contentType;
+		this.quantity = quantity;
+		this.limit = limit;
+		this.aboveHeater = aboveHeater;
+	}
+	
+	public Pot() {
+	}
+	
+	/**
+	 * Adds cups to the container when it has between 0 and 11, maximum 12 cups.
 	 */
 	@Override
 	public void addCups(Container container) {
@@ -14,12 +23,16 @@ public class Pot extends Container {
 			container.quantity++;
 		}
 	}
-
-	/* Change the state of variable aboveHeater */
+	
+	/**
+	 * The method putOnTakeOutPot change the state of variable aboveHeater 
+	 */
 	public void putOnTakeOutPot() {
-		if (aboveHeater)
+		if (aboveHeater) {
 			this.aboveHeater = false;
-		this.aboveHeater = true;
+		}else {
+			this.aboveHeater = true;
+		}
 	}
 
 	/*

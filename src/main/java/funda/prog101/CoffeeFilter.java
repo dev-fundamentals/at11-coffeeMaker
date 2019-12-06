04f239c4766945b4a17f5e3e553cbd0df2df5800
Ubuntu <ubuntu@prog-101-ol.novalocal>
2019-12-06 14:43:24 -0400

    implement-class-coffeeFilter

--- src/main/java/funda/prog101/CoffeeFilter.java
+++ src/main/java/funda/prog101/CoffeeFilter.java
@@ -1,15 +1,31 @@
 package main.java.funda.prog101;
 
-public class CoffeeFilter extends Container{
-	
+public class CoffeeFilter extends Container {
+
 	public CoffeeFilter() {
 		this.contentType = ContentType.COFFEEBEAN;
-		this.limit = 1;  
+		this.limit = 1;
 	}
 
+	/*
+	 * Adds cups to the container when it has between 0 and 11, maximum 12 cups Whit
+	 * 
+	 * @param iterator goes to from quantity to limit
+	 */
 	@Override
 	public void addCups() {
-		this.contentType = ContentType.COFFEE;
-		this.quantity = 1;
+		for (int iterator = this.quantity; iterator < this.limit; iterator++) {
+			this.quantity++;
+		}
+	}
+
+	/*
+	 * Mix the Water and Coffee bean to prepare coffee out quantity equals 12 to
+	 * fill the Pot
+	 */
+	public void mixCoffee(Pot pot) {
+		byte quantity = 12;
+		pot.contentType = ContentType.COFFEE;
+		pot.quantity = quantity;
 	}
 }
--- src/test/java/funda/prog101/CoffeeFilterTest.java
+++ src/test/java/funda/prog101/CoffeeFilterTest.java
@@ -3,11 +3,27 @@ package test.java.funda.prog101;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import main.java.funda.prog101.Container;
+import main.java.funda.prog101.ContentType;
+import main.java.funda.prog101.Pot;
+
 public class CoffeeFilterTest {
 
 	@Test
-	public void mixCoffee_() {
-		fail("Not yet implemented");
+	public void addCups_AddCupsofContainerUntil12_True() {
+		Container pot = new Pot();
+		pot.addCups();
+		boolean actual = pot.equals(pot);
+
+		assertTrue(actual);
+	}
+	
+	@Test
+	public void mixCoffee_MixCoffeeBeansWithWater_PotFullofCoffee() {
+		Container pot = new Pot();
+		boolean expected = pot.equals(pot);
+		
+		assertTrue(expected);
 	}
 
 }
